01e3057ff6d61372f44628678fe3723db4891538
Alberto <Alberto@DESKTOP-UCAT7S6>
2016-09-21 00:14:14 -0300

    Finalizado a cadastro FT e Cadastro Produto

--- teste 8888/JSCadProd.js
+++ teste 8888/JSCadProd.js
@@ -1,10 +1,19 @@
-﻿
-function verificarSenhas(){
-    if (document.form.senha1.value == document.form.senha2.value)
-    {alert ("As duas senhas conferem")
-    }
- 
-    else{
-        alert ("As duas senhas não conferem")
-    }
-}
+﻿$(function () {
+    var divContent = $('#formulario');
+    var botaoAdicionar = $('a[data-id="1"]');
+    var i = 1;
+
+    //Ao clicar em adicionar ele cria uma linha com novos campos
+    $(botaoAdicionar).click(function () {
+        $('<div class="conteudoIndividual"><input type="text" placeholder="Nº do Documento" maxlength="6" name="numeroDocumento' + i + '" required/><select name="tipoDocumento' + i + '" required><option value="" disabled selected>Tipo do Documento</option><option value="01">Volvo</option><option value="02">Saab</option></select><select name="subTipoDocumento' + i + '" required><option value="" disabled selected>Subtipo do Documento</option><option value="01">Volvo</option><option value="02">Saab</option></select><a href="#" id="linkRemover">- Remover Campos</a></div>').appendTo(divContent);
+        $('#removehidden').remove();
+        i++;
+        $('<input type="hidden" name="quantidadeCampos" value="' + i + '" id="removehidden">').appendTo(divContent);
+    });
+
+    //Cliquando em remover a linha é eliminada
+    $('#linkRemover').live('click', function () {
+        $(this).parents('.conteudoIndividual').remove();
+        i--;
+    });
+});
